fd8db7a8b1703ad41240212f3dd9d3878cdaca37
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-12-06 05:39:56 -0500

    deploying-with-firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
 import './App.scss';
 import Home from './Home'
-import LoginPage from './Components/LoginPage';
-import PrivateRoute from './Components/PrivateRoute';
-import AuthButton from './Components/AuthButton';
+// import LoginPage from './Components/LoginPage';
+// import PrivateRoute from './Components/PrivateRoute';
+// import AuthButton from './Components/AuthButton';
 import SigninPage from './Components/SigninPage/SigninPage';
 import { BrowserRouter as Router,Switch, Route, Link } from 'react-router-dom';
-import requests from './requests';
-export const API_URL = 'http://localhost:5000';
+// import requests from './requests';
+// export const API_URL = 'http://localhost:5000';
 function App() {
 
 
   return (
      <Router>
-      <AuthButton />
-      <li>
+      {/* <AuthButton /> */}
+      {/* <li>
               <Link to="/Home">Home Page</Link>
-            </li>
+            </li> */}
             <Switch>
       {/* <Route path="/login" component={LoginPage} /> */}
         <Route path="/login" component={SigninPage} />
-      <PrivateRoute path="/Home" component={Home} />
+              <Route path="/home" component={Home} />
+      {/* <PrivateRoute path="/Home" component={Home} /> */}
       </Switch>
 
      </Router>
---
+++ client/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARfH7_JaOD0mkkCJy_3Nlrwy3bshD88K8",
+  authDomain: "movie-weather-app.firebaseapp.com",
+  projectId: "movie-weather-app",
+  storageBucket: "movie-weather-app.appspot.com",
+  messagingSenderId: "357372821517",
+  appId: "1:357372821517:web:a990de0097b110a9015544"
+};
