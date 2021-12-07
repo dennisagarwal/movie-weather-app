5a5d288d443451c18ba45c6de30749c40ea32932
Deepak Agarwal <47721949+dennisagarwal@users.noreply.github.com>
2021-12-07 05:29:31 -0500

    styling-login-page

--- client/src/Components/SigninPage/SigninPage.js
+++ client/src/Components/SigninPage/SigninPage.js
@@ -1,4 +1,3 @@
-// import {auth} from '../../firebase';
 import React from "react";
 import logo from "../../assets/logo/LogoMakr.png";
 import { Link, useHistory } from "react-router-dom";
@@ -45,8 +44,8 @@ function SigninPage() {
             <div>{error}
             </div>
           )}
-          <form onSubmit={register} className="signUp__form">
-            <h1 className="signin__body--heading3">Log In</h1>
+          <form onSubmit={register} className="signin__form">
+            <h1 className="signin__form--heading1">Log In</h1>
             <input
               ref={emailRef}
               className="signin__detail--forminput"
@@ -68,11 +67,11 @@ function SigninPage() {
               Log In
             </button>
             <h3 className="signin__body--heading3">
-              <span className="signin__body--heading3">Need An Account?</span>
-              <Link to="/signup">Click here to Sign Up.</Link>
+              <span className="signin__form--heading3">Need An Account ?</span>
+              <Link to="/signup" className="signin__form--link">Click here to Sign Up.</Link>
             </h3>
             <h3 className="signin__body--heading3">
-              <Link to="/forgetpassword">Forgot Password?</Link>
+              <Link className="signin__form--link" to="/forgetpassword">Forgot Password?</Link>
             </h3>
           </form>
         </div>
@@ -82,46 +81,3 @@ function SigninPage() {
 }
 
 export default SigninPage;
-
-// import React, { Component } from 'react';
-// import logo from "../../assets/logo/LogoMakr.png"
-// import { useState } from 'react';
-// import "./SigninPage.scss"
-// import Login from '../LoginPage/LoginPage';
-// import SignUpScreen from '../SignupScreen/SignUpScreen';
-
-// function SigninPage(){
-//   const [signIn, setSignIn] = useState(false);
-// // class SigninPage extends Component {
-
-//     return (
-
-//       <div className="signin">
-//        {/* <Login onClick={this.login} /> */}
-//      <img className="signin__logo" src={logo} alt="image logo" />
-//         {/* <p>You must log in to view the page</p> */}
-//         <button onClick={()=>setSignIn(true)} className="signin__button" >Sign in </button>
-//         <div className="signin__gradient"></div>
-//         <div className="signin__body">
-//         {signIn?(
-//           <SignUpScreen />
-//         ):(
-//         <>
-//           <h1 className="signin__body--heading1">Unlimited movies, TV</h1>
-//           <h1 className="signin__body--heading1">shows and more.</h1>
-//         <h2 className="signin__body--heading2">Watch Anywhere.Cancel anytime.</h2>
-//         <h3 className="signin__body--heading3">Ready to watch?Enter your email to create or restart your membership</h3>
-//         <div className="signin__detail">
-//         <form className="signin__detail--form">
-//           <input className="signin__detail--forminput" type="email" placeholder="Email Address" />
-//           <button onClick={()=>setSignIn(true)} className="signin__detail--formbutton">GET STARTED</button>
-//         </form>
-//         </div>
-//         </>
-//         )}
-//         </div>
-//       </div>
-//     );
-//   }
-
-// export default SigninPage;
